fc27bf389281b4589fea7ed4c29217c537db1093
VladislavaYankova <149414999+VladislavaYankova@users.noreply.github.com>
2023-11-13 18:53:27 +0200

    push problem 1

---
+++ textProcessing.lab/printCharacters.js
@@ -0,0 +1,10 @@
+function printCharacters(word) {
+
+    for (let i = 0; i < word.length; i++) {
+        let character = word[i];
+        console.log(character);
+    }
+
+
+}
+printCharacters('AWord');
